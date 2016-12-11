667f129dd85975a9d3b07d11f5f52522d0f9e133
JCHamer <jchamer@umich.edu>
2016-12-10 23:45:37 -0500

    updated the 2nd Order matrices

--- Order2Durs.m
+++ Order2Durs.m
@@ -1,5 +1,5 @@
-%Set up the 1st order MDrkov trDnsition mDtrix thDt shows the probDbilities
-%of jumping to eDch note generDted with D strDight note frequency computDtion
+%Set up the 1st order Markov transition matrix that shows the probabilities
+%of jumping to each note generated with a straight note frequency computation
 
 
 A = zeros(4);
@@ -45,28 +45,28 @@ end
 
 
 
-D = zeros(4);
+A = zeros(4);
 
-D(1,1) = 1/39;	%quDrter quDrter
-D(2,1) = 3/39;	%quDrter 8th
-D(3,1) = 2/39;	%quDrter dotted 8th
-D(4,1) = 0/39;	%quDrter 16th
-D(1,2) = 5/39;	%8th quDrter
-D(2,2) = 21/39;	%8th 8th
-D(3,2) = 1/39;	%8th dotted 8th
-D(4,2) = 0/39;	%8th 16th
-D(1,3) = 0/39;	%dotted 8th quDrter
-D(2,3) = 0/39;	%dotted 8th 8th
-D(3,3) = 0/39;	%dotted 8th dotted 8th
-D(4,3) = 3/39;	%dotted 8th 16th
-D(1,4) = 0/39;	%16th quDrter
-D(2,4) = 3/39;	%16th 8th
-D(3,4) = 0/39;	%16th dotted 8th
-D(4,4) = 0/39;	%16th 16th
+A(1,1) = 1/6;	%quarter quarter
+A(2,1) = 3/6;	%quarter 8th
+A(3,1) = 2/6;	%quarter dotted 8th
+A(4,1) = 0/6;	%quarter 16th
+A(1,2) = 5/27;	%8th quarter
+A(2,2) = 21/27;	%8th 8th
+A(3,2) = 1/27;	%8th dotted 8th
+A(4,2) = 0/27;	%8th 16th
+A(1,3) = 0/3;	%dotted 8th quarter
+A(2,3) = 0/3;	%dotted 8th 8th
+A(3,3) = 0/3;	%dotted 8th dotted 8th
+A(4,3) = 3/3;	%dotted 8th 16th
+A(1,4) = 0/3;	%16th quarter
+A(2,4) = 3/3;	%16th 8th
+A(3,4) = 0/3;	%16th dotted 8th
+A(4,4) = 0/3;	%16th 16th
 
-%D 1X1 mDtrix of rDndom #'s from 7
+%A 1X1 matrix of random #'s from 7
 notes = zeros(4,30);
-%stDrt = rDndi(4,1,1);
+%start = randi(4,1,1);
 
 
 
--- readInNotes.py
+++ readInNotes.py
@@ -14,12 +14,28 @@ durs = "888888888888884888888888888848168888416884416888888888848"
 
 pitches = "abcdefg"
 pitchDict = {'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5, 'f' : 6, 'g' : 7}
-total = 0
+durations = "4816"
+durDict = {'4' : 1, '8' : 2, '1' : 3, '6' : 4}
 
+totals = [0]*7
 for prevPitch in pitches:
 	for currPitch in pitches:
 		occur = notes.count(prevPitch + currPitch)
-		total += occur
+		totals[pitchDict[prevPitch] - 1] += occur
 		print("A(%i,%i) = %i/xx; %%%s %s" % (pitchDict[currPitch], pitchDict[prevPitch], occur, str(prevPitch), str(currPitch)))
 
-print(total)
+for total in totals:
+	print(total)
+
+print
+
+totals = [0]*4
+for prevDur in durations:
+	for currDur in durations:
+		occur = durs.count(prevDur + currDur)
+		totals[durDict[prevDur] - 1] += occur
+		print("A(%i,%i) = %i/xx; %%%s %s" % (durDict[currDur], durDict[prevDur], occur, str(prevDur), str(currDur)))
+
+for total in totals:
+	print(total)
+
